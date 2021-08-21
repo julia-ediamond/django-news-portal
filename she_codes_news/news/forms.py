6efa828f2ff0a1da715d1d10fd2ca2bdd1be252a
iusharnina@gmail.com <zjulia679@gmail.com>
2021-08-21 13:03:22 +1000

    Added image field to form

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -6,6 +6,6 @@ from .models import NewsStory
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ['title', 'pub_date', 'content']
+        fields = ['title', 'pub_date','image_url', 'content']
         widgets = {'pub_date': forms.DateInput(format=(
             '%m/%d/%Y'), attrs={'class': 'form-control', 'placeholder': 'Selecta date', 'type': 'date'}), }
